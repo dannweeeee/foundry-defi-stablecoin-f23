d8d85007ad9eeb2292b903fc6c6806e82f562362
Dann Wee <dannweezj@gmail.com>
2023-09-21 14:44:23 +0700

    dscEngineTest unit test

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -3,5 +3,17 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "forge-std/Test.sol";
+import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {DecentralisedStableCoin} from "../../src/DecentralisedStableCoin.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
 
-contract DSCEngineTest is Test {}
+contract DSCEngineTest is Test {
+    DeployDSC deployer;
+    DecentralisedStableCoin dsc;
+    DSCEngine dsce;
+
+    function setUp() public {
+        deployer = new DeployDSC();
+        (dsc, dsce) = deployer.run();
+    }
+}
